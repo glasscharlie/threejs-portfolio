b985eb16e4aaa88fbb1843acf4929e43edbf73a2
glasscharlie <charlie-glass@live.com>
2025-06-24 15:52:24 -0700

    Changed Wording
    Changed some wording

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
-      {/* <Projects /> */}
+      <Projects />
       <WorkExperience />
       <Contact />
       <Footer />
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,12 +3,6 @@ import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { useMediaQuery } from 'react-responsive';
 import { PerspectiveCamera } from '@react-three/drei';
-
-import Cube from '../components/Cube.jsx';
-import Rings from '../components/Rings.jsx';
-import ReactLogo from '../components/ReactLogo.jsx';
-import Button from '../components/Button.jsx';
-import Target from '../components/Target.jsx';
 import CanvasLoader from '../components/Loading.jsx';
 import HeroCamera from '../components/HeroCamera.jsx';
 import { calculateSizes } from '../constants/index.js';
@@ -26,7 +20,7 @@ const Hero = () => {
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
-          Hi, I am Charlie 
+          Hi, I'm Charlie 
         </p>
         <p className="hero_tag text-gray_gradient">Software Engineer</p>
       </div>
