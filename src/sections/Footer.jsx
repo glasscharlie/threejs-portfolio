b21f2bd0df89c83d6e4a40d30b68bea75dc0e3e2
glasscharlie <charlie-glass@live.com>
2025-06-25 11:23:54 -0700

    Footer Buttons
    added a tags to the footer logos to make them buttons

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,16 +2,19 @@ const Footer = () => {
   return (
     <footer className="c-space pt-7 pb-3 border-t border-black-300 flex-wrap gap-5 ">
 
-      <div className="flex gap-3 align-center justify-center">
+      <div className="flex gap-10 align-center justify-center" >
+        <a href="https://github.com/glasscharlie" target="_blank">
         <div className="social-icon">
+          
           <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+          
         </div>
+        </a>
+        <a href="https://www.linkedin.com/in/charles-glass1/" target="_blank">
         <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
+          <img src="/assets/linkedin.png" alt="LinkedIn" className="w-1/2 h-1/2" />
         </div>
-        <div className="social-icon">
-          <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-        </div>
+        </a>
       </div>
 
 
